07c057d572acd31b932ad32a4903d48d010f4ec1
annamartorell <55982572+annamartorell@users.noreply.github.com>
2019-11-08 13:21:12 +0100

    Add files via upload

---
+++ js/codi.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    var texto =
+        document.getElementById("text");
+        TweenLite.to(texto, 3, {
+        backgroundColor: "#8AC6C3",
+        color: "black"
+    });
+}
